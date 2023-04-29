715dee38b1890a5e3e2476dfa6f23b8366da0d1d
colatti2017@gmail.com <e107ti@etec.sp.gov.br>
2023-04-28 21:25:36 -0300

    exercicio Resolvido

---
+++ Program-1.cs
@@ -0,0 +1,17 @@
+﻿// Determine se um número é múltiplo de outro.
+Console.WriteLine("--- Multiplo ---\n");
+
+Console.Write("Digite um numero...........: ");
+int n1 = int.Parse(Console.ReadLine()!); 
+
+Console.Write("Avaliar se é múltiplo de...: ");
+int n2 = int.Parse(Console.ReadLine()!); 
+
+if(n1 % n2 == 0)
+{
+    Console.WriteLine($"{n1} é multiplo de {n2}");
+}
+else
+{
+    Console.WriteLine($"{n1} não é multiplo de {n2}");
+}
